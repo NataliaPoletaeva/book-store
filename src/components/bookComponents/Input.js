fe6be96df5dd3d9a6d6f29b6ff00f118589a76e2
NataliaPoletaeva <natfly799@gmail.com>
2022-01-26 22:51:58 +0500

    Update add and remove features

--- src/components/bookComponents/Input.js
+++ src/components/bookComponents/Input.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 
-const Input = () => (
-  <div>
-    <form className="input-form">
-      <input typeof="text" className="add-book" placeholder="Book title" required />
-      <input typeof="text" className="add-author" placeholder="Book author" required />
-      <select className="book-list">
-        <option> Romance </option>
-        <option> Science Fiction </option>
-        <option> Action </option>
-        <option> Fantasy </option>
-        <option> History </option>
-      </select>
-      <button type="submit" className="add-btn">Add book</button>
+const Input = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addBookToStore = (e) => {
+    e.preventDefault();
+    if (title === '' || author === '') return;
+    const newBook = {
+      id: Date.now().toString(),
+      title,
+      author,
+    };
+
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form className="form">
+      <input
+        className="input-title"
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        className="input-author"
+        type="text"
+        placeholder="Book author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        required
+      />
+      <button className="add-book-btn" type="submit" onClick={addBookToStore}>Add Book</button>
     </form>
-  </div>
-);
+  );
+};
 
 export default Input;
